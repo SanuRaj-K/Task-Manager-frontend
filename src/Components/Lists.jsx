7316b57c2f346207723693bd9d5ee15306c1bc6b
SanuRaj-K <sanusrk29@gmail.com>
2024-06-11 14:39:22 +0530

    fix

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import AddSection from './AddSection'
+import React from "react";
+import AddSection from "./AddSection";
 
 function Lists() {
   return (
     <div>
-        <AddSection/>
+      <AddSection />
+
+      <div className=" grid grid-cols-3 ">
+        <div className=" ">
+          <h1>All</h1>
+        </div>
+        <div>
+          
+          <h1>Completed</h1>
+        </div>
+        <div>
+          
+          <h1>Pending</h1>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Lists
+export default Lists;
